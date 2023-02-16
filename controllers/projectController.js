0df96cf974f5014761be4797d2bdac82281c050f
shm_usuf <shmusuf@gmail.com>
2023-02-16 12:06:58 +0600

    projects sorting added

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,7 +3,7 @@ const Project = require("../models/projectModel");
 
 //get all projects
 const getAllProjects = async (req, res) => {
-  const projects = await Project.find({});
+  const projects = await Project.find({}).sort({ createdAt: -1 }); //descending ordered newly added on top
 
   res.status(200).json(projects);
 };
