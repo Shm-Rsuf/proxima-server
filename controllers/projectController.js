856eb3fa7f5913e901428c2dc3c5063a23228047
shm_usuf <shmusuf@gmail.com>
2023-02-20 16:46:51 +0600

    updated context added

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -92,11 +92,49 @@ const deleteProject = async (req, res) => {
 const updateProject = async (req, res) => {
   const { id } = req.params;
 
+  const { title, tech, budget, manager, duration, dev } = req.body;
+
+  const emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+
+  if (!tech) {
+    emptyFields.push("tech");
+  }
+
+  if (!budget) {
+    emptyFields.push("budget");
+  }
+
+  if (!manager) {
+    emptyFields.push("manager");
+  }
+
+  if (!duration) {
+    emptyFields.push("duration");
+  }
+
+  if (!dev) {
+    emptyFields.push("dev");
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
+
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "Invalid id" });
   }
 
-  const project = await Project.findOneAndUpdate({ _id: id }, { ...req.body });
+  const project = await Project.findOneAndUpdate(
+    { _id: id },
+    { ...req.body },
+    { new: true }
+  );
 
   if (!project) {
     return res.status(400).json({ error: "Project not found" });
