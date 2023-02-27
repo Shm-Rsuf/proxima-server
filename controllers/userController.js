aa9bc1ccc6ea15c98d0d9c6b9e4a878dda308524
shm_usuf <shmusuf@gmail.com>
2023-02-27 18:05:39 +0600

    login coltroller added

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,17 @@ const createToken = (id) => {
 
 //login user
 const loginUser = async (req, res) => {
-  res.json({ message: "login" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    //create a token
+    const token = await createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //signup user
